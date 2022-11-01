8df4724509a121ba6d75ed1c05ead2e37378f012
technorahul <rahul.sahu@technoloader.com>
2022-11-01 18:50:44 +0530

    initail commit

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class HomeController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function index()
+    {
+        return view('home');
+    }
+
+    multi-Authentication created
+    frontend user created and reset password
+    role and permission (progress)
+
+
+    public function adminHome()
+    {
+        return view('adminHome');
+    }
+}
